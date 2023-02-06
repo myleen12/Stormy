2eef512d48b53be800b9a0d51c9f1afc68268f8c
myleen12 <myleen.santana12@icloud.com>
2023-02-06 00:17:13 -0800

    updated days HTML, fixed bug in API

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,13 @@ var search = document.getElementById("search");
 var searchBtn = document.getElementById("wee-btn");
 var byeCity = document.getElementById("city");
 var weekday
-searchBtn.addEventListener("click", All);
+searchBtn.addEventListener("click", display);
 
-function All() {
+function display() {
+    console.log(search.value);
   var urlGeo =
-    'http://api.openweathermap.org/geo/1.0/direct?q=' + search.value + '&limit=1&appid=' + APIkey + '';
+    'http://api.openweathermap.org/geo/1.0/direct?q=' + search.value + '&limit=1&appid=91739a09f6b5cd87988f3fbe99ab2d30'
+
   fetch(urlGeo)
     .then(function (response) {
       return response.json();
@@ -16,29 +18,29 @@ function All() {
       console.log(data);
        var lat = data[0].lat;
        var lon = data[0].lon;
-       var urlforcast = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon='+ lon + '&appid=' + APIkey + '&units=metric';
+       var urlforcast = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon='+ lon + '&appid=' + APIkey + '&units=imperial';
 
       fetch(urlforcast)
         .then(function (response) {
-          return response.json;
+          return response.json();
         })
         .then(function (data) {
           console.log(data);
           byeCity.textContent = data.city.name;
           for (var i = 0; i < 6; i++) {
-            document.getElementById("t" + i + "").textContent =
+            document.getElementById("temp-" + i + "").textContent =
               "Temp: " + Number(data.list[i].main.temp).toFixed(0) + "°";
           }
           for (var i = 0; i < 6; i++) {
-            document.getElementById("w" + i + "").textContent =
+            document.getElementById("wind-" + i + "").textContent =
               "wind: " + Number(data.list[i].main.temp).toFixed(0) + "mph";
           }
           for (var i = 0; i < 6; i++) {
-            document.getElementById("h" + i + "").textContent =
+            document.getElementById("humidity-" + i + "").textContent =
               "Humidity: " + Number(data.list[i].main.temp).toFixed(0) + "%";
           }
           for (var i = 0; i < 6; i++) {
-            document.getElementById("icon" + i + "").src =
+            document.getElementById("icon-" + i + "").src =
               "http://openweathermap.org/img/wn/" +
               data.list[i].weather[0].icon +
               ".png";
@@ -48,13 +50,13 @@ function All() {
 }
 
 var weekday = [
-  monent().format("dddd"),
-  monent().add(1, "d").format("dddd"),
-  monent().add(2, "d").format("dddd"),
-  monent().add(3, "d").format("dddd"),
-  monent().add(4, "d").format("dddd"),
-  monent().add(5, "d").format("dddd"),
-  monent().add(6, "d").format("dddd"),
+  moment().format("dddd"),
+  moment().add(1, "d").format("dddd"),
+  moment().add(2, "d").format("dddd"),
+  moment().add(3, "d").format("dddd"),
+  moment().add(4, "d").format("dddd"),
+  moment().add(5, "d").format("dddd"),
+  moment().add(6, "d").format("dddd"),
 ];
 for (i = 0; i < 6; i++) {
   document.getElementById('date-' + i + "").textContent = weekday[i];
