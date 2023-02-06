57ca3ba7e511006c74361495577ae6525382697c
myleen12 <myleen.santana12@icloud.com>
2023-02-05 19:46:35 -0800

    updated HTML  creat weater border section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,66 @@
-var API = '37fe2c7a05cab49a132f4779b0884fb3'
-var card 
+var APIkey = '91739a09f6b5cd87988f3fbe99ab2d30'
+var livecity = '';
+var lon;
+var lat;
+var urlGeo = 'http://api.openweathermap.org/geo/1.0/direct?lat=${lat}&lon=${lon}&appid=${APIkey}&units=${metric}&lang=${lang}';
+var search = document.getElementById('#search');
+var searchBtn = document.getElementById('.btn')
+var urlforcast = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIkey}&units=metric`;'
+
+
+
+
+
+function geo() {
+    urlGeo = 'http://api.openweathermap.org/geo/1.0/direct?lat=${lat}&lon=${lon}&appid=${APIkey}&units=${metric}&lang=${lang}';
+    console.log('hello')
+fetch(urlGeo)
+      .then
+
+
+}
+
+
+window.addEventListener('load',()=>{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
